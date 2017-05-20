eb126daa407b85cfda8337667b28583b97de5232
Jesse Liang <jesseliang@ymail.com>
2017-05-20 15:35:15 -0700

    example

---
+++ example.py
@@ -0,0 +1,38 @@
+import requests
+import base64
+import sys
+import json
+
+class Nomad:
+
+	def __init__(self):
+		self.url = 'http://54.208.17.147/zappos/detect/'
+		# self.url = 'http://localhost/zappos/detect/'
+
+	def detect(self, image_dir):
+		with open(image_dir, "rb") as f:
+			data = f.read()
+			base64 = data.encode("base64")
+			data = { 'base64': base64 }
+			response = requests.post(self.url, json=data)
+			return response
+
+def containsHotDog(results):
+	for res in result["results"]:
+		if res["name"] == "hot dog" and res["confidence"] > 50:
+			return True
+		else: 
+			return False
+
+if len(sys.argv) < 2:
+	print "missing image directory"
+else: 
+	api = Nomad()
+	result = json.loads(api.detect(sys.argv[1]).text)
+	
+	if containsHotDog(result):
+		print "Hotdog"
+	else: 
+		print "Not hotdog"
+
+
