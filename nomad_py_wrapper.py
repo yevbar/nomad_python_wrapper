3c51518c1fcb9a2735fa20212d61c2aec1e43a3a
Devin Mui <devinmui@yahoo.com>
2017-05-20 12:21:51 -0700

    ur retarded

--- nomad_py_wrapper.py
+++ nomad_py_wrapper.py
@@ -1,16 +1,18 @@
 import requests
 import base64
 
-class nomad(object):
-	
-	def _init_(self):
+class Nomad:
+	def __init__(self):
 		self.url = 'http://54.208.17.147/zappos/detect/'
+		# self.url = 'http://localhost/zappos/detect/'
 
 	def detect(self, image_dir):
 		with open(image_dir, "rb") as f:
-		    data = f.read()
-		    base64 = data.encode("base64")
-		    data = "{ base64: " + base64 + "}"
-		    response = requests.post(self.url, data = data)
-		    return response
-
+			data = f.read()
+			base64 = data.encode("base64")
+			data = { 'base64': base64 }
+			response = requests.post(self.url, json=data)
+			return response
+# testing
+# api = Nomad()
+# print api.detect("f.jpg").text
